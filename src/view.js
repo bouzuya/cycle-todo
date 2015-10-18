d8932a459fb5f5e43b3bcc3913482100aca17a9d
bouzuya <m@bouzuya.net>
2015-10-18 13:38:08 +0900

    clear input.title when pressing enter key

--- src/view.js
+++ src/view.js
@@ -11,7 +11,7 @@ function renderTodos(todos) {
 export default function(state$) {
   const vtree$ = state$.map(({ count, title, todos }) => {
     return h('div', [
-      h('input.title'),
+      h('input.title', { value: '' }),
       h('p', [
         'count:',
         h('span.count', '' + count)
