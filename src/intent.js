3eb6241a925c40828f00237ff17a274e18dbce36
bouzuya <m@bouzuya.net>
2015-10-18 15:22:23 +0900

    change addTodo action parameter format

--- src/intent.js
+++ src/intent.js
@@ -6,8 +6,9 @@ export default function(responses) {
         const enter = 13;
         return e.keyCode === enter;
       })
-      .map((e) => e.target.value.trim())
-      .filter((i) => i.length > 0),
+      .map(e => e.target.value.trim())
+      .filter(i => i.length > 0)
+      .map(title => ({ title })),
     toggleCompleted$: DOM.select('input[type=checkbox]').events('click')
       .map((e) => {
         const id = parseInt(e.target.value, 10);
--- src/model.js
+++ src/model.js
@@ -5,32 +5,24 @@ export default function(actions) {
   const actions$ = Rx.Observable
     .merge(
       addTodo$
-      .map((title) => {
-        return (todos) => {
-          const completed = false;
-          const id = todos.length + 1;
-          const todo = { title, id, completed };
-          return todos.concat([todo]);
-        };
+      .map(({ title }) => (todos) => {
+        const completed = false;
+        const id = todos.length + 1;
+        const todo = { title, id, completed };
+        return todos.concat([todo]);
       }),
       toggleCompleted$
-      .map(({ id, completed }) => {
-        return (todos) => {
-          const todo = todos.filter((i) => i.id === id)[0];
-          todo.completed = completed;
-          return todos;
-        };
+      .map(({ id, completed }) => (todos) => {
+        const todo = todos.filter((i) => i.id === id)[0];
+        todo.completed = completed;
+        return todos;
       })
     );
   const state = [];
   const state$ = Rx.Observable
     .just(state)
     .merge(actions$)
-    .scan((todos, action) => {
-      return action(todos);
-    })
-    .map((todos) => {
-      return { todos };
-    });
+    .scan((todos, action) => action(todos))
+    .map(todos => ({ todos }))
   return state$;
 }
